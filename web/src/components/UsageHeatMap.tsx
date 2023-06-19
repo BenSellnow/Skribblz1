403967132b086bc5db47c11134dc2a3fea1f8377
Benjamin Sellnow <116616278+BenSellnow@users.noreply.github.com>
2023-06-19 03:50:06 -0500

    Update UsageHeatMap.tsx

--- web/src/components/UsageHeatMap.tsx
+++ web/src/components/UsageHeatMap.tsx
@@ -162,7 +162,9 @@ const UsageHeatMap = () => {
       </div>
       <p className="w-full pl-4 text-xs -mt-2 mb-3 text-gray-400 dark:text-zinc-400">
         <span className="font-medium text-gray-500 dark:text-zinc-300 number">{memoAmount} </span>
-        {memoAmount === 1 ? "skribbl" : "skribblz"} on {getDateString(createdDays)}
+        {memoAmount === 1 ? "Skribbl in" : "Skribblz in"}{" "}
+        <span className="font-medium text-gray-500 dark:text-zinc-300">{createdDays} </span>
+        {createdDays === 1 ? t("heatmap.day", tMemoInOpts) : t("heatmap.days", tMemoInOpts)}
       </p>
     </>
   );
